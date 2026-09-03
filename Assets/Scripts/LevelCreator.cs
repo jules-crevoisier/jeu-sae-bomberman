5ec2f59cec7a64dc70b6b83334fd11e603649965
Jordan <98762843+Squeishy@users.noreply.github.com>
2026-09-03 14:54:19 +0200

    Added Basic Main Menu Navigation

---
+++ Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour
+{
+    [SerializeField] private string levelJson;
+
+    [ContextMenu("Generate Level")]
+    void GenerateLevel()
+    {
+        // Implementation for generating the level
+    }
+}
